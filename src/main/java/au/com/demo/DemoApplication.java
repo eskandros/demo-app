d025adadcd721acceba9ca1929ed0d6767ec8029
meskandros <meskandros@bigpond.com>
2018-05-18 13:06:03 +1000

    Creating a new project

--- src/main/java/au/com/demo/DemoApplication.java
+++ src/main/java/au/com/demo/DemoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IssueTrackerApplication {
+public class DemoApplication {
 
     public static void main(final String[] args) {
-        SpringApplication.run(IssueTrackerApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
