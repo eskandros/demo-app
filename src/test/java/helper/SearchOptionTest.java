3d0fe4ffec4a15c4a2d0d9f3af590d13fe80e46c
meskandros <meskandros@bigpond.com>
2018-05-18 12:59:53 +1000

    Creating a new project

--- src/test/java/helper/SearchOptionTest.java
+++
@@ -1,7 +0,0 @@
-package helper;
-
-/**
- * Created by mseskander on 27/04/2018.
- */
-public class SearchOptionTest {
-}
--- src/test/java/util/UtilsTest.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-/**
- * Created by mseskander on 27/04/2018.
- */
-public class UtilsTest {
-}
